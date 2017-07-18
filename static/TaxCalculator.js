6baf1c3f6055422380be549667cb965cc4b4015f
Siya Kali <ingamerc@gmail.com>
2017-07-18 13:57:15 +0200

    Currently working on programming in travel allowance and over 65 age calculations. Minor tweaks necessary to syntax when the result is a string instead of a number

--- static/TaxCalculator.js
+++ static/TaxCalculator.js
@@ -2,24 +2,45 @@ $(window).on("load", function() {
 
     console.log("window loaded");
 
+    document.getElementById('under65Box').checked = true;
+    document.getElementById('salary').defaultValue = 50000;
+    document.getElementById('bonus').defaultValue = 0;
+    document.getElementById('travelAllowance').defaultValue = 0;
+    document.getElementById('pension').defaultValue = 500;
+    document.getElementById('annuity').defaultValue = 0;
+
+
+    // Function to return the retired status
+    var getRetiredStatus = function(){
+
+        if (document.getElementById('between65and75Box').checked) {
+            return "semiretired";
+        } else if (document.getElementById('over75Box').checked) {
+            return "retired";
+        }
+
+        return "unretired";
+
+    };
+
     // Function to store the salary input value
     var getSalary = function(){
-        return (Number(document.getElementById('salary').value));
+        return Number(document.getElementById('salary').value);
     };
 
     // Function to store the bonus input value
     var getBonus = function(){
-        return (Number(document.getElementById('bonus').value));
+        return Number(document.getElementById('bonus').value);
     };
 
     // Function to store the pension input value
     var getPension = function(){
-        return (Number(document.getElementById('pension').value));
+        return Number(document.getElementById('pension').value);
     };
 
     // Function to store the annuity input value
     var getAnnuity = function(){
-        return (Number(document.getElementById('annuity').value));
+        return Number(document.getElementById('annuity').value);
     };
 
     // Function to calculate the net salary.
@@ -27,35 +48,97 @@ $(window).on("load", function() {
     // When bonus is NOT included, add it to salary
     var salaryNet = function(){
         if (document.getElementById('bonusIncludedBox').checked) {
-            return (getSalary() - getBonus() - getPension() - getAnnuity());
-        } else {
-            return (getSalary() - getPension() - getAnnuity());
+            return getSalary() - getBonus() - getPension() - getAnnuity();
         }
+        return getSalary() - getPension() - getAnnuity();
+    };
+
+    var salaryWithBonus = function(){
+        return salaryNet() + getBonus();
     };
 
     // Function to calculate tax on net salary
     var calculateTaxOnSalary = function(){
 
-        console.log("net salary" + salaryNet());
+        console.log("net salary = " + salaryNet());
 
-        if (salaryNet() < 0) {
-            return ("Please enter a positve value as salary cannot be negative");
-        } else if (salaryNet() <= 75750) {
-            return ("You are not eligible for taxation");
-        } else if (salaryNet() <= 189880) {
-            return ((salaryNet() - 75750) * 18 / 100);
-        } else if (salaryNet() <= 296540) {
-            return ((salaryNet() - 189880) * 26 / 100 + 34178 - 13635);
-        } else if (salaryNet() <= 410460) {
-            return ((salaryNet() - 296540) * 31 / 100 + 61910 - 13635);
-        } else if (salaryNet() <= 555600) {
-            return ((salaryNet() - 410460) * 36 / 100 + 97225 - 13635);
-        } else if (salaryNet() <= 708310) {
-            return ((salaryNet() - 555600) * 39 / 100 + 149475 - 13635);
-        } else if (salaryNet() < 1500000) {
-            return ((salaryNet() - 708310) * 41 / 100 + 209032 - 13635);
-        } else if (salaryNet() > 1500000) {
-            return ((salaryNet() - 1500000) * 45 / 100 + 533625 - 13635);
+        if (getRetiredStatus() === "semiretired") {
+            console.log("ageBetween65and75");
+
+            var net = salaryNet();
+
+            if (net < 0) {
+                return "Please enter a positve value as salary cannot be negative";
+            } else if (net <= 117300) {
+                return "You are not eligible for taxation";
+            } else if (net <= 189880) {
+                return (net - 75750) * 18 / 100;
+            } else if (net <= 296540) {
+                return (net - 189880) * 26 / 100 + 34178 - 13635;
+            } else if (net <= 410460) {
+                return (net - 296540) * 31 / 100 + 61910 - 13635;
+            } else if (net <= 555600) {
+                return (net - 410460) * 36 / 100 + 97225 - 13635;
+            } else if (net <= 708310) {
+                return (net - 555600) * 39 / 100 + 149475 - 13635;
+            } else if (net < 1500000) {
+                return (net - 708310) * 41 / 100 + 209032 - 13635;
+            } else if (net > 1500000) {
+                return (net - 1500000) * 45 / 100 + 533625 - 13635;
+            }
+
+        } else if (getRetiredStatus() === "retired") {
+
+            console.log("ageOver75");
+
+            var net = salaryNet();
+
+            if (net < 0) {
+                return "Please enter a positve value as salary cannot be negative";
+            } else if (net <= 131500) {
+                return "You are not eligible for taxation";
+            } else if (net <= 189880) {
+                return (net - 75750) * 18 / 100;
+            } else if (net <= 296540) {
+                return (net - 189880) * 26 / 100 + 34178 - 13635;
+            } else if (net <= 410460) {
+                return (net - 296540) * 31 / 100 + 61910 - 13635;
+            } else if (net <= 555600) {
+                return (net - 410460) * 36 / 100 + 97225 - 13635;
+            } else if (net <= 708310) {
+                return (net - 555600) * 39 / 100 + 149475 - 13635;
+            } else if (net < 1500000) {
+                return (net - 708310) * 41 / 100 + 209032 - 13635;
+            } else if (net > 1500000) {
+                return (net - 1500000) * 45 / 100 + 533625 - 13635;
+            }
+
+        } else {
+
+            console.log ("under65")
+
+            var net = salaryNet();
+
+            if (net < 0) {
+                return "Please enter a positve value as salary cannot be negative";
+            } else if (net <= 75750) {
+                return "You are not eligible for taxation";
+            } else if (net <= 189880) {
+                return (net - 75750) * 18 / 100;
+            } else if (net <= 296540) {
+                return (net - 189880) * 26 / 100 + 34178 - 13635;
+            } else if (net <= 410460) {
+                return (net - 296540) * 31 / 100 + 61910 - 13635;
+            } else if (net <= 555600) {
+                return (net - 410460) * 36 / 100 + 97225 - 13635;
+            } else if (net <= 708310) {
+                return (net - 555600) * 39 / 100 + 149475 - 13635;
+            } else if (net < 1500000) {
+                return (net - 708310) * 41 / 100 + 209032 - 13635;
+            } else if (net > 1500000) {
+                return (net - 1500000) * 45 / 100 + 533625 - 13635;
+            }
+
         }
 
     };
@@ -63,65 +146,73 @@ $(window).on("load", function() {
     // Function to calculate tax on net salary WITH BONUS
     var calculateTaxOnSalaryWithBonus = function(){
 
-        var salaryWithBonus = function(){
-            return(salaryNet() + getBonus());
-        };
+        var salaryBonus = salaryWithBonus();
 
-        console.log("net salary with bonus" + salaryWithBonus());
+        console.log("net salary with bonus = " + salaryWithBonus());
 
-        if (salaryWithBonus() < 0) {
-            return ("Please enter a positve value as salary cannot be negative");
-        } else if (salaryWithBonus() <= 75750) {
-            return ("You are not eligible for taxation");
-        } else if (salaryWithBonus() <= 189880) {
-            return ((salaryWithBonus() - 75750) * 18 / 100);
-        } else if (salaryWithBonus() <= 296540) {
-            return ((salaryWithBonus() - 188000) * 26 / 100 + 33840 - 13500);
-        } else if (salaryWithBonus() <= 410460) {
-            return ((salaryWithBonus() - 296540) * 31 / 100 + 61910 - 13635);
-        } else if (salaryWithBonus() <= 555600) {
-            return ((salaryWithBonus() - 410460) * 36 / 100 + 97225 - 13635);
-        } else if (salaryWithBonus() <= 708310) {
-            return ((salaryWithBonus() - 555600) * 39 / 100 + 149475 - 13635);
-        } else if (salaryWithBonus() < 1500000) {
-            return ((salaryWithBonus() - 708310) * 41 / 100 + 209032 - 13635);
-        } else if (salaryWithBonus() > 1500000) {
-            return ((salaryWithBonus() - 1500000) * 45 / 100 + 533625 - 13635);
+        if (salaryBonus < 0) {
+            return "Please enter a positve value as salary cannot be negative";
+        } else if (salaryBonus <= 75750) {
+            return "You are not eligible for taxation";
+        } else if (salaryBonus <= 189880) {
+            return (salaryBonus - 75750) * 18 / 100;
+        } else if (salaryBonus <= 296540) {
+            return (salaryBonus - 189880) * 26 / 100 + 34178 - 13635;
+        } else if (salaryBonus <= 410460) {
+            return (salaryBonus - 296540) * 31 / 100 + 61910 - 13635;
+        } else if (salaryBonus <= 555600) {
+            return (salaryBonus - 410460) * 36 / 100 + 97225 - 13635;
+        } else if (salaryBonus <= 708310) {
+            return (salaryBonus - 555600) * 39 / 100 + 149475 - 13635;
+        } else if (salaryBonus < 1500000) {
+            return (salaryBonus - 708310) * 41 / 100 + 209032 - 13635;
+        } else if (salaryBonus > 1500000) {
+            return (salaryBonus - 1500000) * 45 / 100 + 533625 - 13635;
         }
 
     };
 
     // Function to calculate tax on bonus
     var calculateTaxOnBonus = function(){
-        return (calculateTaxOnSalaryWithBonus() - calculateTaxOnSalary());
+
+        console.log("tax on salary with bonus = " + calculateTaxOnSalaryWithBonus());
+
+        if (getBonus() === 0) {
+            return "You do not have a taxable bonus amount"
+        }
+
+        return calculateTaxOnSalaryWithBonus() - calculateTaxOnSalary();
     };
 
     // Function to calculate total tax (tax on salary + tax on bonus)
     var calculateTotalTax = function(){
-        if (salaryNet() <= 75750) {
-            return (calculateTaxOnBonus());
-        } else if (salaryNet() > 75750)
-            return (calculateTaxOnSalary() + calculateTaxOnBonus());
+
+        if (getBonus() === 0) {
+            return calculateTaxOnSalary()
+        }
+
+        return calculateTaxOnSalary() + calculateTaxOnBonus();
+
     };
 
+    // Function to calculate take home pay
     var calculateTakeHomePay = function(){
-        if (salaryNet() <= 75750) {
-            return (salaryNet());
-        } else if (salaryNet() > 75750)
-            return (salaryNet() + getBonus() - calculateTotalTax());
+
+        if (getBonus() === 0) {
+            return salaryNet()
+        }
+
+        return salaryNet() + getBonus() - calculateTotalTax();
+
     };
-
-
 
     document.getElementById('calculateTaxButton').addEventListener('click', function(){
 
-        document.getElementById('totalTaxResult').innerHTML = calculateTotalTax();
-        document.getElementById('taxOnBasicResult').innerHTML = calculateTaxOnSalary();
-        document.getElementById('taxOnBonusResult').innerHTML = calculateTaxOnBonus();
-        document.getElementById('takeHomePayResult').innerHTML = calculateTakeHomePay();
+        document.getElementById('totalTaxResult').innerHTML = "R " + calculateTotalTax();
+        document.getElementById('taxOnBasicResult').innerHTML = "R " + calculateTaxOnSalary();
+        document.getElementById('taxOnBonusResult').innerHTML = "R " + calculateTaxOnBonus();
+        document.getElementById('takeHomePayResult').innerHTML = "R " + calculateTakeHomePay();
 
     });
-
-
 
 });
